ac55fc6bd4c34febc84b2d0e53ebbcfc9e0d15c9
saif <saif.saffi@gmail.com>
2024-05-06 02:04:13 +0500

    final push

---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import {createRouter} from 'vue-router'; 
+
+export default createRouter({
+    routes: [
+        {
+            path: '/',
+            name: 'Home',
+            component: HomePage
+        },
+        {
+            path: '/',
+            name: 'Home',
+            component: HomePage
+        },
+        {
+            path: '/',
+            name: 'Home',
+            component: HomePage
+        },
+    ],
+}); 
